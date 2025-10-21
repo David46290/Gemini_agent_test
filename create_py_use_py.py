3a1b758f8d6f90ab2f9f34ff479daea172798256
David46290 <141000324+David46290@users.noreply.github.com>
2025-10-21 09:10:55 +0800

    Update create_py_use_py.py
    make it more flexible to definite function

--- create_py_use_py.py
+++ create_py_use_py.py
@@ -1,27 +1,30 @@
-
 import subprocess
 
-
-
-# main.py
-new_func = """
-def run():
-    return 'function successed'//2
-"""
-
-the_main = """
-if __name__ == "__main__":
+def make_new_code(new_addon='', addon_name=''):
+    func_def = f"def {addon_name}():"
+    
+    # main.py
+    the_main = f"""if __name__ == "__main__":\n
     try:
-        print(run())
+        print({addon_name}())
     except Exception as e:
-        print(f"|error encountered| {e}")
-"""
+        print("|error encountered|:" + e)
+    """
+    new_code = func_def + '\n' + new_addon + '\n' + the_main
+    
+    with open("new_script.py", "w", encoding="utf-8") as f:
+        f.write(new_code)
 
-new_code = new_func + '\n' + the_main
+def run_new_code():
+    result = subprocess.run(["python", "new_script.py"], capture_output=True, text=True) # string
+    error_alarm = "|error encountered|:"
+    print(result.stdout.strip())
 
-with open("new_script.py", "w", encoding="utf-8") as f:
-    f.write(new_code)
-
-
-result = subprocess.run(["python", "new_script.py"], capture_output=True, text=True)
-print(result.stdout.strip())
+if __name__ == "__main__":
+    addon_name = 'new_func'
+    new_addon = """
+    return 'function successed'
+    """
+    make_new_code(new_addon, addon_name)
+    run_new_code()
+    
