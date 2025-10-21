20e3f8fa7ce0a4ea37e3b3effab2ef0bdc938d63
David46290 <141000324+David46290@users.noreply.github.com>
2025-10-21 08:48:48 +0800

    Update create_py_use_py.py
    make it able to only add a new function

--- create_py_use_py.py
+++ create_py_use_py.py
@@ -4,17 +4,24 @@ import subprocess
 
 
 # main.py
-new_code = """
+new_func = """
 def run():
-    return "Hello from new script!"
-    
-if __name__ == "__main__":
-    print(run())
+    return 'function successed'//2
 """
+
+the_main = """
+if __name__ == "__main__":
+    try:
+        print(run())
+    except Exception as e:
+        print(f"|error encountered| {e}")
+"""
+
+new_code = new_func + '\n' + the_main
 
 with open("new_script.py", "w", encoding="utf-8") as f:
     f.write(new_code)
 
 
 result = subprocess.run(["python", "new_script.py"], capture_output=True, text=True)
-print("回傳值:", result.stdout.strip())
+print(result.stdout.strip())
